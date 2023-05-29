492a17fbff7d887b27435ef959ab1f81a2e22436
Beni <beni@Benis-MBP.home>
2023-05-29 20:31:06 +0100

    final commit

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Beni Ibeh on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
